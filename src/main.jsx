1ea641ac06658a32422861fa8fa08bedf7ac843c
JrMatosCoder <sk8.merp@gmail.com>
2023-06-27 22:12:42 -0300

    alteração nas rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 //NAVBAR
 import Navbar from "./components/navbar";
@@ -10,12 +10,12 @@ import Campanha from "./routes/campanha";
 import Deuses from "./routes/deuses";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/campanha" element={<Campanha />} />
-      <Route path="/deuses" element={<Deuses />} />
+      <Route path="campanha" element={<Campanha />} />
+      <Route path="deuses" element={<Deuses />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // ...outras configurações...
+  server: {
+    // Configuração do servidor
+    hmr: {
+      // habilitar suporte ao react-router-dom
+      host: "localhost",
+    },
+    proxy: {
+      // proxy para direcionar as requisições para a rota curinga
+      "/api": {
+        target: "http://localhost:5000",
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    fs: {
+      // fallback para index.html
+      strict: false,
+    },
+  },
+});
